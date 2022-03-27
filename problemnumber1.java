13e52d638b834f30302902d8804fd6211de8352d
Reynard Timotius <91584193+Xhourstoprogram@users.noreply.github.com>
2022-03-27 22:21:48 +0700

    Add files via upload

---
+++ problemnumber1.java
@@ -0,0 +1,28 @@
+package com.OOP;
+
+import java.util.Scanner;
+
+public class problemnumber1 {
+    public static void main(String[] args) throws Exception {
+        int numItems;
+        int[] items;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number of items: ");
+        numItems = in.nextInt();
+        items = new int[numItems];
+        if (items.length > 0) {
+
+            for (int i = 0; i < items.length; ++i) {
+                System.out.print("Enter the value of all items (separated by space) : ");
+                items[i] = in.nextInt();
+            }
+        }
+        String  values = "" + items[0];
+        for (int i = 1; i < items.length; ++i) {
+            values = values + "," + items[i];
+        }
+
+        System.out.println("The values are: [" + values + "]");
+        }
+    }
+
